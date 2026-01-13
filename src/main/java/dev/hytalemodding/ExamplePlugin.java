29ad683a53f4352cf322cfb0d5b11027cfc1f632
ItsNeil17 <neil@willofsteel.me>
2026-01-13 22:24:04 +0530

    feat: add example event
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- src/main/java/dev/hytalemodding/ExamplePlugin.java
+++ src/main/java/dev/hytalemodding/ExamplePlugin.java
@@ -1,8 +1,10 @@
 package dev.hytalemodding;
 
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dev.hytalemodding.commands.ExampleCommand;
+import dev.hytalemodding.events.ExampleEvent;
 
 import javax.annotation.Nonnull;
 
@@ -15,6 +17,7 @@ public class ExamplePlugin extends JavaPlugin {
     @Override
     protected void setup() {
         this.getCommandRegistry().registerCommand(new ExampleCommand("example", "An example command"));
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
     }
 
     @Override
