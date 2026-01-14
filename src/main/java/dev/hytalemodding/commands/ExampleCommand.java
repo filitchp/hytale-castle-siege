e09b504a2a47a5fe9845fac1bcfd243cdeee5c1d
ItsNeil17 <neil@willofsteel.me>
2026-01-14 17:33:45 +0530

    feat: use  instead of
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>
    
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/dev/hytalemodding/commands/ExampleCommand.java
+++ src/main/java/dev/hytalemodding/commands/ExampleCommand.java
@@ -1,19 +1,24 @@
 package dev.hytalemodding.commands;
 
 import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
 
-public class ExampleCommand extends CommandBase {
+public class ExampleCommand extends AbstractCommand {
 
     public ExampleCommand(String name, String description) {
         super(name, description);
     }
 
+    @Nullable
     @Override
-    protected void executeSync(@Nonnull CommandContext context) {
+    protected CompletableFuture<Void> execute(@Nonnull CommandContext context) {
         context.sendMessage(Message.raw("Hello from ExampleCommand!"));
+        return CompletableFuture.completedFuture(null);
     }
+
 }
